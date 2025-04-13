ff45273d78f6cc00f343b218baed50c09c6a314e
farhan baber <farhanbaber965@gmail.com>
2025-04-13 11:58:59 +0500

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.circle-progress').forEach(circle => {
+    const percent = circle.getAttribute('data-percent');
+    const circleProgress = circle.querySelector('.progress');
+    const circumference = 2 * Math.PI * 54; // Radius is 54
+    circleProgress.style.strokeDasharray = `${circumference} ${circumference}`;
+    circleProgress.style.strokeDashoffset = circumference - (percent / 100) * circumference;
+});
+  
