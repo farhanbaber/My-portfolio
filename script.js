481f8aa9c4b425a75a2f2e99f504bda3a9735b09
farhan baber <farhanbaber965@gmail.com>
2025-04-27 22:34:42 +0500

    again changes

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// document.querySelectorAll('.circle-progress').forEach(circle => {
-//     const percent = circle.getAttribute('data-percent');
-//     const circleProgress = circle.querySelector('.progress');
-//     const circumference = 2 * Math.PI * 54; // Radius is 54
-//     circleProgress.style.strokeDasharray = `${circumference} ${circumference}`;
-//     circleProgress.style.strokeDashoffset = circumference - (percent / 100) * circumference;
-// });
 
 
 document.querySelectorAll('.circle-progress').forEach(progress => {
@@ -19,3 +12,17 @@ document.querySelectorAll('.circle-progress').forEach(progress => {
     const offset = circumference - (percent / 100) * circumference;
     circle.style.strokeDashoffset = offset;
 });
+
+// loader
+
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+
+    setTimeout(() => {
+      loader.style.opacity = '0';
+    }, 1000); 
+
+    setTimeout(() => {
+      loader.style.display = 'none';
+    }, 2000); 
+  });
